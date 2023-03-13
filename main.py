5aba5397f2ca3cd71a30a370ab483b385a5aed8e
qwegat <63029208+qwegat@users.noreply.github.com>
2023-03-13 20:21:11 +0900

    a

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ def searchMusic(words):
         for track in res['tracks']['items']:
             id_list.append(track['id'])
             meta_list.append({
-                "genre":",".join(track["genres"]),
                 "artist":",".join([x["name"] for x in track["artists"]]),
                 "release_date": track["release_date"]
             })
