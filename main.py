6806ac27fb75b3ab4bd54819a9f87617be3c9ec0
qwegat <63029208+qwegat@users.noreply.github.com>
2023-03-13 22:00:51 +0900

    a

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def getSearchWords(theme):
         frequency_penalty=0,
         presence_penalty=0
     )
-    st.text(res["choices"][0]["text"])
+    st.text(sKillReg.sub("", res["choices"][0]["text"]))
     return sKillReg.sub("", res["choices"][0]["text"]).split("\n")
 
 
