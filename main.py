68cbf6940ae3972fcae22f12abbea8e799d32500
qwegat <63029208+qwegat@users.noreply.github.com>
2023-03-13 19:01:33 +0900

    test

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import openai
+import os
+import re
 
+openai.api_key = os.getenv("CHATGPT_API_KEY")
+
+sKillReg = re.compile("^\d+\. ")
+
+def getSearchWords(theme):
+
+    res = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"10 words to search for on Spotify when you want to create a '{theme}' themed songs playlist:\n1. ",
+        suffix="",
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    print(sKillReg.sub("",res["choices"][0]))
